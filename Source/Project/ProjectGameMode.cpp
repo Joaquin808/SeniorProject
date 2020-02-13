d9203fc644091e66acbdb6804b7bd0f37ceb3c26
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2020-02-12 22:01:35 -0500

    Moved stuff around and deleted unnecessary stuff

--- Source/Project/ProjectGameMode.cpp
+++ Source/Project/ProjectGameMode.cpp
@@ -9,9 +9,9 @@ AProjectGameMode::AProjectGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	/**static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
-	HUDClass = AProjectHUD::StaticClass();
+	HUDClass = AProjectHUD::StaticClass();**/
 }
--- Source/Project/ProjectHUD.cpp
+++ Source/Project/ProjectHUD.cpp
@@ -10,8 +10,8 @@
 AProjectHUD::AProjectHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
-	CrosshairTex = CrosshairTexObj.Object;
+	//static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	//CrosshairTex = CrosshairTexObj.Object;
 }
 
 
@@ -22,7 +22,7 @@ void AProjectHUD::DrawHUD()
 	// Draw very simple crosshair
 
 	// find center of the Canvas
-	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+	/**const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
 	const FVector2D CrosshairDrawPosition( (Center.X),
@@ -31,5 +31,5 @@ void AProjectHUD::DrawHUD()
 	// draw the crosshair
 	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+	Canvas->DrawItem( TileItem );**/
 }
