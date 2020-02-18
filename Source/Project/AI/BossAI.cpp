478d643713d197ea0ce55679eadcba731b965505
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2020-02-18 13:45:22 -0500

    Created BossAI
    Created the BossAI class and the animation blueprint

---
+++ Source/Project/AI/BossAI.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BossAI.h"
+#include "Engine/Engine.h"
+#include "Pickups/Weapons/Sword.h"
+
+// Sets default values
+ABossAI::ABossAI()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABossAI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	Weapon = GetWorld()->SpawnActor<ASword>(WeaponClass, FVector(-10.0f, 20.0f, 10.0f), FRotator(0.0f, 0.0f, -90.0f), SpawnParams);
+
+	if (Weapon)
+	{
+		Weapon->SetActorEnableCollision(false);
+		FString AttachmentSocket = "Socket_Weapon";
+		FName SocketName = FName(*AttachmentSocket);
+		Weapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, SocketName);
+	}
+}
+
+// Called every frame
+void ABossAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
